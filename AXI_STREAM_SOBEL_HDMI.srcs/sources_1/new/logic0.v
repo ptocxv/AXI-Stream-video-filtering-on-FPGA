116befbbee14b9a6caef5d00d52944d3240f0398
PhatTo <huuphat.to@student.kuleuven.be>
2026-08-14 15:44:38 +0200

    custom high/low logic replacing built in constant ips

---
+++ AXI_STREAM_SOBEL_HDMI.srcs/sources_1/new/logic0.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/14/2026 03:25:22 PM
+// Design Name: 
+// Module Name: logic0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module logic0(
+    output val0
+    );
+    
+    assign val0 = 1'b0;
+    
+endmodule
